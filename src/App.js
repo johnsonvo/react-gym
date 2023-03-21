f55f2f378d7c22a0f9bb8598bdf0f00fda67dfd7
Johnson Vo <johnsonmvo@gmail.com>
2023-03-21 13:03:31 -0700

    creates baseline functional react app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./App.css";
+import ExerciseDetail from "./scenes/ExerciseDetail";
+import Home from "./scenes/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Box width='400px'>
-      Navbar
+    <Box width='400px' sx={{ width: { xl: "1488px" } }} m='auto'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/exercise/:id' element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 };
